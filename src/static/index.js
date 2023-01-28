724ba4c1d86e9238591b7ef6fa208a99e412ead4
alexcoder04 <alexcoder04@protonmail.com>
2023-01-28 14:14:35 +0100

    fix download bug now downloads correct code if the user changed the text but not created a new code

--- src/static/index.js
+++ src/static/index.js
@@ -1,37 +1,54 @@
-let button = document.querySelector(".button");
+
+let createBtn = document.querySelector(".button");
+let downloadBtn = document.querySelector(".download");
 let input = document.querySelector(".text");
 let img = document.querySelector(".image");
-let dbutton = document.querySelector(".download");
 
-button.addEventListener("click", (ev) => {
-	if(input.value === ""){
-		return;
-	}
+// creates a qr code from the value of the input field and shows it
+async function createQRCode() {
+	let text = input.value;
 
-	fetch("/post-qr", {
-		method: 'POST',
+	return fetch("/post-qr", {
+		method: "POST",
 		headers: {
-			'Content-Type': 'application/json'
+			"Content-Type": "application/json"
 		},
-		body: JSON.stringify({data: input.value})
+		body: JSON.stringify({data: text})
 	}).then(async(res) => {
 		let buffer = await res.arrayBuffer();
 		var base64 = btoa(
 			new Uint8Array(buffer)
-			  .reduce((data, byte) => data + String.fromCharCode(byte), '')
+			  .reduce((data, byte) => data + String.fromCharCode(byte), "")
 		  );
-		img.src = 'data:image/png;base64,' + base64;
+		img.src = "data:image/png;base64," + base64;
+		img.dataset.text = text;
 	})
-})
+}
 
-dbutton.addEventListener("click", (ev) => {
-	if(new URL(img.src).pathname == "/"){
+createBtn.addEventListener("click", async () => {
+	// no need to create empty qr codes
+	if(input.value === ""){
 		return;
 	}
-	
-	console.log(new URL(img.src).pathname);
 
-	let a = document.createElement('a');
+	await createQRCode();
+})
+
+downloadBtn.addEventListener("click", async () => {
+	let text = input.value;
+
+	// no need to download empty codes
+	if (text === "") {
+		return;
+	}
+
+	// check whether the generated code is up-to-date with what is in the input field
+	if (img.dataset.text != text) {
+		await createQRCode();
+	}
+	
+	// download the code
+	let a = document.createElement("a");
 	a.href = img.src;
 	a.download = "code.png";
 	document.body.appendChild(a);
