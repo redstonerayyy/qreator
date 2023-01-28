38aa2ca2a558772551910e8612b7796c6796255d
alexcoder04 <alexcoder04@protonmail.com>
2023-01-28 13:55:47 +0100

    save code image to temp directory

--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
+#!/usr/bin/env python3
+
 from flask import Flask, send_file, request
+import tempfile
+import os
 import qrcode
+
+CODE_FILE = os.path.join(tempfile.gettempdir(), "qrcode.png")
 
 app = Flask(__name__)
 
 @app.route("/post-qr", methods=["POST"])
 def getqr():
 	img = qrcode.make(request.json["data"])
-	img.save("code.png")
-	return send_file("code.png", mimetype='image/png')
+	img.save(CODE_FILE)
+	return send_file(CODE_FILE, mimetype="image/png")
 
 @app.route('/')
 def hello_world():
-	return app.send_static_file('index.html')
+	return app.send_static_file("index.html")
 
 if __name__ == "__main__":
 	app.run()
